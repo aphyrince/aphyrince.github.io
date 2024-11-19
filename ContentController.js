0eff8f650efc3125b37ca40e5e332a2cd85ce366
aphyrince <dkswjdgh01@naver.com>
2024-11-19 22:06:36 +0900

    페이지 이동 구현

--- ContentController.js
+++ ContentController.js
@@ -1,7 +1,20 @@
-
-
 class ContentController {
     constructor() {
+        this.nav = document.getElementById("nav");
+        this.content = document.getElementById("content");
 
+        for (let i = 0; i < this.nav.children.length; i++) {
+            this.nav.children[i].addEventListener('click', () => {
+                // 모든 페이지를 'hide' 클래스를 추가하여 숨김
+                for (let page of this.content.children) {
+                    page.classList.add('hide');
+                }
+
+                // 클릭된 메뉴에 해당하는 페이지를 보이도록 설정
+                const pageIndex = i;
+                const activePage = this.content.children[pageIndex];
+                activePage.classList.remove('hide');
+            });
+        }
     }
 }
